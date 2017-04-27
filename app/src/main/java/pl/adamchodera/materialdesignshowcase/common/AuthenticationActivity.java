9282c910780e8ce73cc4a3f7eec2220e4d510732
Adam Chodera <adam@chodera.com.pl>
2017-04-27 20:59:19 +0200

    Introduce AuthenticationActivity which is intended for login & registration

---
+++ app/src/main/java/pl/adamchodera/materialdesignshowcase/common/AuthenticationActivity.java
@@ -0,0 +1,37 @@
+package pl.adamchodera.materialdesignshowcase.common;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.BindView;
+import pl.adamchodera.materialdesignshowcase.R;
+
+public abstract class AuthenticationActivity extends AppCompatActivity {
+
+    @BindView(R.id.progress_bar)
+    protected View progressView;
+    @BindView(R.id.authentication_form)
+    protected AuthenticationFormView formView;
+
+    protected abstract void triggerAuthentication();
+
+    protected void initFormView() {
+        formView.setOnAuthenticationAttemptListener(new AuthenticationFormView.OnAuthenticationAttemptListener() {
+            @Override
+            public void onAuthenticationAttempt() {
+                if (formView.isValid()) {
+                    triggerAuthentication();
+                }
+            }
+        });
+    }
+
+    /**
+     * Shows the progress bar and hides the form.
+     */
+    protected void showProgress(final boolean show) {
+        progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        formView.setVisibility(show ? View.GONE : View.VISIBLE);
+    }
+
+}
--- app/src/main/java/pl/adamchodera/materialdesignshowcase/registration/RegisterActivity.java
+++ app/src/main/java/pl/adamchodera/materialdesignshowcase/registration/RegisterActivity.java
@@ -1,24 +1,15 @@
 package pl.adamchodera.materialdesignshowcase.registration;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import pl.adamchodera.materialdesignshowcase.R;
-import pl.adamchodera.materialdesignshowcase.common.AuthenticationFormView;
+import pl.adamchodera.materialdesignshowcase.common.AuthenticationActivity;
 
 /**
  * A screen that offers registration via email & password.
  */
-public class RegisterActivity extends AppCompatActivity {
-
-    @BindView(R.id.progress_bar)
-    View progressView;
-
-    @BindView(R.id.authentication_form)
-    AuthenticationFormView formView;
+public class RegisterActivity extends AuthenticationActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +17,12 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
 
-        initFormView();
+        super.initFormView();
     }
 
-    private void initFormView() {
-        formView.setOnAuthenticationAttemptListener(new AuthenticationFormView.OnAuthenticationAttemptListener() {
-            @Override
-            public void onAuthenticationAttempt() {
-                if (formView.isValid()) {
-                    showProgress(true);
-                    // TODO send request to backend; redirect to main screen for now
-                }
-            }
-        });
-    }
-
-    /**
-     * Shows the progress bar and hides the form.
-     */
-    private void showProgress(final boolean show) {
-        progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-        formView.setVisibility(show ? View.GONE : View.VISIBLE);
+    @Override
+    protected void triggerAuthentication() {
+        super.showProgress(true);
     }
 }
 
