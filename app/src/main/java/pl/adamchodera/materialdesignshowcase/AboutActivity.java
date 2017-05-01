4cb7e0549338e5ba7082a0f82cbbd38d35e87b4b
Adam Chodera <adam@chodera.com.pl>
2017-05-01 23:15:24 +0200

    Implement sharing link to the app and back navigation

--- app/src/main/java/pl/adamchodera/materialdesignshowcase/AboutActivity.java
+++ app/src/main/java/pl/adamchodera/materialdesignshowcase/AboutActivity.java
@@ -1,10 +1,11 @@
 package pl.adamchodera.materialdesignshowcase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
@@ -15,14 +16,36 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.activity_about_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Share link to this app", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               shareAppLink();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void shareAppLink() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, getLinkToGooglePlay());
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.share_link)));
+    }
+
+    private String getLinkToGooglePlay() {
+        return "Check out this app: https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID;
     }
 }
