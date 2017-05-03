b7eb4425ef55178b533c591dc54b299e56d8ef8b
Adam Chodera <adam@chodera.com.pl>
2017-05-03 22:24:37 +0200

    Display fragment’s number inside fragments

--- app/src/main/java/pl/adamchodera/materialdesignshowcase/dashboard/DashboardFragment.java
+++ app/src/main/java/pl/adamchodera/materialdesignshowcase/dashboard/DashboardFragment.java
@@ -7,40 +7,52 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import pl.adamchodera.materialdesignshowcase.R;
 
 public class DashboardFragment extends Fragment {
 
-    private static final String ARG_POSITION = "ARG_POSITION";
+    private static final String ARG_FRAGMENT_NUMBER = "ARG_FRAGMENT_NUMBER";
 
     @BindView(R.id.fragment_dashboard_text_view)
     TextView textView;
 
-    public static DashboardFragment newInstance(final int position) {
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_POSITION, position);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    @BindString(R.string.large_text)
+    String largeText;
+
+    private int fragmentNumber;
 
     public DashboardFragment() {
+    }
+
+    public static DashboardFragment newInstance(final int fragmentNumber) {
+        DashboardFragment fragment = new DashboardFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_FRAGMENT_NUMBER, fragmentNumber);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fragmentNumber =  getArguments().getInt(ARG_FRAGMENT_NUMBER);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        ButterKnife.bind(view);
+        ButterKnife.bind(this, view);
 
+        final String newLine = System.getProperty("line.separator"); // alternative: "\n"
+        final String fragmentTitle = "This is fragment number: " + fragmentNumber;
+        final String fragmentContent = fragmentTitle + newLine + newLine + largeText;
 
+        textView.setText(fragmentContent);
 
         return view;
     }
