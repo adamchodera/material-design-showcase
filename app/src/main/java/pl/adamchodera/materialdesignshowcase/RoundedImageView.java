28eace44cffe916d476e9d49db7ac60864fecffd
Adam Chodera <adam@chodera.com.pl>
2017-05-04 22:55:25 +0200

    Add rounded img and styling to navigation view

---
+++ app/src/main/java/pl/adamchodera/materialdesignshowcase/RoundedImageView.java
@@ -0,0 +1,39 @@
+package pl.adamchodera.materialdesignshowcase;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.util.AttributeSet;
+
+public class RoundedImageView extends android.support.v7.widget.AppCompatImageView {
+
+    public RoundedImageView(final Context context) {
+        super(context);
+        init();
+    }
+
+    public RoundedImageView(final Context context, AttributeSet arAttributeSet) {
+        super(context, arAttributeSet);
+
+        init();
+    }
+
+    public RoundedImageView(Context context, AttributeSet attributeSet, int defStyleAttr) {
+        super(context, attributeSet, defStyleAttr);
+
+        init();
+    }
+
+    private void init() {
+        Resources res = getResources();
+        Bitmap src = BitmapFactory.decodeResource(res, R.drawable.img_my_photo);
+
+        RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory.create(res, src);
+        drawable.setCircular(true);
+
+        setImageDrawable(drawable);
+    }
+}
